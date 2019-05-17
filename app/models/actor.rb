4e821af8e731981d363f128ed47b88007ea00396
Berlinta <nashfft@gmail.com>
2019-05-16 20:13:55 -0700

    test

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,5 +5,11 @@ class Actor < ActiveRecord::Base
    def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+   def list_roles
+     characters.collect do |character|
+       "#{character.name} - #{character.show.name}"
+     end
+  end
   
 end
